c297f09708b520ae43481e020ffa920248ff22b0
mogwms <mogwms@gmail.com>
2025-10-21 15:50:10 -0400

    "update"

---
+++ BasicMathSolver.java
@@ -0,0 +1,25 @@
+public class BasicMathSolver {
+    public double Add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double Subtract(double num1, double num2) {
+        if (num1 > num2 ) {
+            return num1 - num2;
+        } else {
+            return num2 - num1;
+        }
+    }
+
+    public double Divide(double num1, double num2) {
+        if (num1 > num2) {
+            return num1 / num2;
+        } else {
+            return num2 / num1;
+        }
+    }
+
+    public double Multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+}
